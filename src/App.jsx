af5fb1f9843a4bc98eb933c2ca5f5c4fd6b57b7e
Nethu <neth.hettihewa@gmail.com>
2024-06-24 09:24:19 +0530

    Initial Files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>HEYYY</h1>
+      <nav className='navBar'>
+        <a href="https://react.dev/">Home</a>
+        <a href="https://react.dev/">Contact</a>
+        <a href="https://react.dev/">Portfolio</a>
+
+      </nav>
+     </div>
+     <div className="content2">
+
+     </div>
+    </>
+     
+
+  )
+}
+
+export default App
